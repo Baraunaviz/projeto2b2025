afb3a98fa23cbd8f7517b9903c9aa3c439c28167
Baraunaviz <rm94349@estudante.fieb.edu.br>
2025-03-10 07:58:38 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Component } from "react";
+
+class App extends Component {
+
+  render(){
+
+    return (
+      <div>
+        <h3>Projeto 2B</h3>
+        <p>Componente de classe</p>
+      </div>
+    )
+  }
+}
+
+export default App
